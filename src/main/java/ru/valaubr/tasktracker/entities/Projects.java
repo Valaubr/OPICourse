3ef14fd372ff7e0662b6c1dacc7ad4e23c590378
Valaubr <stalker039906@gmail.com>
2022-12-26 18:00:24 +0300

    Added project managment

--- src/main/java/ru/valaubr/tasktracker/entities/Projects.java
+++ src/main/java/ru/valaubr/tasktracker/entities/Projects.java
@@ -2,6 +2,7 @@ package ru.valaubr.tasktracker.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,18 +10,15 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Projects {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "project_users",
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "api_user_id"))
     private List<ApiUser> customers;
-
-    public Projects(String name) {
-        this.name = name;
-    }
 }
